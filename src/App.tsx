f0a1013a6e800a79fe111a883bdef3be22f96565
fdacio <fdacio@gmail.com>
2025-01-17 22:02:35 -0300

    Primeiro Commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import shopLogo from './assets/shop.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+          <img src={shopLogo} className="logo" alt="Shop logo" />
+          <h1>Shop App</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
